b7c7c4092652b6ea27a2475a20375307864ca7f7
Kelsy Vaughn <angelstate@gmail.com>
2020-01-16 22:32:25 -0800

    Update underbar.js

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,7 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+    return val;
   };
 
   /**
@@ -37,6 +38,13 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    if (n === 0) {
+      return [];
+    } else if (n === undefined) {
+      return array[array.length-1];
+    }else {
+      return array.slice(-n);
+    }
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -45,6 +53,15 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if(Array.isArray(collection)) {
+      for (var i=0;i<collection.length; i++) {
+        iterator(collection[i], i, collection)
+      } 
+    } else {
+      for (var key in collection) {
+        iterator(collection[key], key, collection) 
+      }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -66,16 +83,38 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var array = [];
+    _.each(collection, function(item, i, c) {
+      if (test(item,i,c)) {
+        array.push(item);
+      }
+    })
+    return array;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    return _.filter(collection, function (item, i, c) {
+      return !test(item, i, c);
+    })
   };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array, isSorted, iterator) {
+    if(typeof iterator !== 'function') {
+      iterator = _.identity
+    }
+    var unique = {};
+    _.each(array, function (item) {
+      if (unique[iterator(item)] === undefined) {
+        unique[iterator(item)] = item;
+      }
+    })
+    return _.map(unique, function(val) {
+      return val;
+    })
   };
 
 
